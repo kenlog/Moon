31b82a6f90f02fdf2962340e80b696544a017c87
Valentino Pesce <valentino@iltuobrand.it>
2019-01-05 10:59:59 +0100

    Fix: logo not clickable close #2

--- Template/layout/header/title.php
+++ Template/layout/header/title.php
@@ -1,24 +1,20 @@
 <?php global $themeMoonConfig; ?>
-<div class="header">
-    <?php if (!isset($themeMoonConfig['logo'])) : ?>
-        <?= $this->url->link('K<span>B</span>', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
-    <?php else: ?>
-        <?= $this->url->link('<img src="'.$this->url->dir().'plugins/Moon/Assets/images/'.$themeMoonConfig['logo'].'" />', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
-    <?php endif ?>
-    <?php if (!isset($themeMoonConfig['HeadingTitleColor'])) : ?>
-    <h2>
-    <?php else: ?>
-    <h2 style="color:<?= $themeMoonConfig['HeadingTitleColor'] ?>">
-    <?php endif ?>
+    <span class="logo">
+        <?php if (!isset($themeMoonConfig['logo'])) : ?>
+            <?= $this->url->link('K<span>B</span>', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
+        <?php else: ?>
+            <?= $this->url->link('<img src="'.$this->url->dir().'plugins/Moon/Assets/images/'.$themeMoonConfig['logo'].'" style="float: left;max-height: 40px;margin-top: -7px;margin-right: 10px;" />', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
+        <?php endif ?>
+    </span>
+<h1>
+    <span class="title">
         <?php if (! empty($project) && ! empty($task)): ?>
             <?= $this->url->link($this->text->e($project['name']), 'BoardViewController', 'show', array('project_id' => $project['id'])) ?>
         <?php else: ?>
             <?= $this->text->e($title) ?>
         <?php endif ?>
-        <small>
-            <?php if (! empty($description)): ?>
-                <?= $this->app->tooltipHTML($description) ?>
-            <?php endif ?>
-        </small>
-    </h2>
-</div>
+    </span>
+    <?php if (! empty($description)): ?>
+        <?= $this->app->tooltipHTML($description) ?>
+    <?php endif ?>
+</h1>
