0478d1ffc8a53015b634680b89b619360721c149
Valentino Pesce <valentino@iltuobrand.it>
2023-07-08 12:58:26 +0200

    FIX plugin folder

--- Plugin.php
+++ Plugin.php
@@ -11,37 +11,35 @@ class Plugin extends Base
     {
         global $themeMoonConfig;
 
-        if (file_exists(DATA_DIR . '/files/Moon/config.php')) 
-        {
+        $pluginFolder = basename(PLUGINS_DIR);
+
+        if (file_exists(DATA_DIR . '/files/Moon/config.php')) {
             require_once(DATA_DIR . '/files/Moon/config.php');
-        } else { 
-            mkdir(DATA_DIR . '/files/Moon/Assets/images', 0755, true); 
-            copy('plugins/Moon/config.php', DATA_DIR . '/files/Moon/config.php');
-            copy('plugins/Moon/Assets/images/brand-logo.png', DATA_DIR . '/files/Moon/Assets/images/brand-logo.png');
+        } else {
+            mkdir(DATA_DIR . '/files/Moon/Assets/images', 0755, true);
+            copy($pluginFolder . '/Moon/config.php', DATA_DIR . '/files/Moon/config.php');
+            copy($pluginFolder . '/Moon/Assets/images/brand-logo.png', DATA_DIR . '/files/Moon/Assets/images/brand-logo.png');
         }
-		
-		if (file_exists('plugins/Customizer'))
-        {
+
+        if (file_exists($pluginFolder . '/Customizer')) {
             $this->template->setTemplateOverride('header/title', 'Moon:layout/header/customizerTitle');
             $this->template->setTemplateOverride('header', 'Moon:header');
             $this->template->setTemplateOverride('layout', 'Moon:layout');
-        } 
-			elseif (isset($themeMoonConfig['logo'])) 
-        {
+        } elseif (isset($themeMoonConfig['logo'])) {
             $this->template->setTemplateOverride('header/title', 'Moon:layout/header/title');
             $this->template->setTemplateOverride('header', 'Moon:header');
             $this->template->setTemplateOverride('layout', 'Moon:layout');
         }
-		
-        $this->hook->on("template:layout:css", array("template" => "plugins/Moon/Assets/css/moon.css"));
 
-        $this->hook->on("template:layout:css", array("template" => "plugins/Moon/Assets/css/prism.css"));
+        $this->hook->on("template:layout:css", array("template" => $pluginFolder . "/Moon/Assets/css/moon.css"));
 
-        $this->hook->on('template:layout:js', array('template' => 'plugins/Moon/Assets/js/clipboard.min.js'));
+        $this->hook->on("template:layout:css", array("template" => $pluginFolder . "/Moon/Assets/css/prism.css"));
 
-        $this->hook->on('template:layout:js', array('template' => 'plugins/Moon/Assets/js/prism.js'));
-    
-        $this->hook->on('template:layout:js', array('template' => 'plugins/Moon/Assets/js/moon.js'));
+        $this->hook->on('template:layout:js', array('template' => $pluginFolder . '/Moon/Assets/js/clipboard.min.js'));
+
+        $this->hook->on('template:layout:js', array('template' => $pluginFolder . '/Moon/Assets/js/prism.js'));
+
+        $this->hook->on('template:layout:js', array('template' => $pluginFolder . '/Moon/Assets/js/moon.js'));
     }
 
     public function getPluginName()
@@ -61,7 +59,7 @@ class Plugin extends Base
 
     public function getPluginVersion()
     {
-        return '1.4.6';
+        return '1.4.7';
     }
 
     public function getCompatibleVersion()
@@ -73,5 +71,4 @@ class Plugin extends Base
     {
         return 'https://github.com/kenlog/Moon';
     }
-
 }
